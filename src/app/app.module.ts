e39e3d00fbce8e849372b63917e55dd49b557e49
robunye <robunye@gmail.com>
2022-05-19 10:32:05 -0300

    Criação do Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { NovoProdutoComponent } from './components/novo-produto/novo-produto.component';
+import { ErrorComponent } from './components/error/error.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    CadastroComponent,
+    ProdutosComponent,
+    FeedbackComponent,
+    NovoProdutoComponent,
+    ErrorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
